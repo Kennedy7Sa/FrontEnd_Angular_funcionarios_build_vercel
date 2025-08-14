a90923007a976958603e7d0c34ab4db647d28faf
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2025-08-14 14:28:15 -0300

    MUDANÇA NO LINK DE ENVIRONMENT

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    apiUrl : "https://localhost:7151/api/"
-
+    //apiUrl : "https://localhost:7151/api/"
+apiUrl : "https://apifuncionarios-deploy-render.onrender.com/api/"
 
 };
